6f0b7ebd8f5ff9b591cf29f2950e9503eaffd1d7
monica438 <monica14dominguez@gmail.com>
2026-04-10 16:23:01 +0200

    Delete lib directory

--- lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'main_app.dart' as app_main;
-
-Future<void> main() async {
-  await app_main.MainApp.main();
-}
--- lib/runtime_transform.dart
+++
@@ -1,8 +0,0 @@
-class RuntimeTransform {
-  final double initialX;
-  final double initialY;
-  double x;
-  double y;
-
-  RuntimeTransform(this.x, this.y) : initialX = x, initialY = y;
-}
